1b3c03051d5656b8c200628207beceaec1461a47
Fylhan <fylhan@gmail.com>
2015-06-25 18:25:19 +0200

    Clarify command line

--- docs/Command line.md
+++ docs/Command line.md
@@ -16,8 +16,12 @@ At the root directory of your project, where qompoter.json is available, simply 
 
 Some options are availables:
 
-* **-r, --repo**		Select a repository path as a location for dependency research. It is used in addition of the "repositories" field in qompoter.json. E.g. *repo/repositories/vendor name/project name*
-* **--vendir-dir**	Pick another vendor directory as "vendor"
-* **--no-dev**		Don't retrieve dependencies for development listed in "require-dev"
-* **-v, --version**	Display this Qompoter version
-* **-h, --help**		Display help
+* **-r, --repo**                        Select a repository path as a location for package discovery. It is used in addition of the "repositories" field in qompoter.json. E.g. gits=https://github.com.
+* **-f, --file <filename>**             Pick another filename as "qompoter.json"
+* **-d, --working-dir <directory>**     Pick another working directory as the current one.
+* **--vendir-dir <directory>**          Pick another vendor directory as "vendor".
+* **--no-dev**                          Skip packages for development listed in require-dev
+* **-g, --global**                      Install the packages globaly on the machine instead of localy
+* **-V, --verbose**                     Increase verbosity of messages
+* **-v, --version**                     Display this Qompoter version
+* **-h, --help**                        Display help
--- src/commandline.cpp
+++ src/commandline.cpp
@@ -16,20 +16,22 @@ CommandLineParseResult Qompoter::parseCommandLine(QCommandLineParser &parser, Qu
     availableActions<<"install"<<"update"<<"build";
     parser.addPositionalArgument("action", QObject::tr("Action to execute: ")+availableActions.join(", "));
     parser.setSingleDashWordOptionMode(QCommandLineParser::ParseAsLongOptions);
-    const QCommandLineOption helpOption = parser.addHelpOption();
-    const QCommandLineOption versionOption = parser.addVersionOption();
-    QCommandLineOption verboseOption(QStringList() << "V" << "verbose", QObject::tr("Increase verbosity of messages"));
-    parser.addOption(verboseOption);
-    QCommandLineOption globalOption(QStringList() << "g" << "global", QObject::tr("Install the dependency on the machine, instead of localy, for the current project only"));
-    parser.addOption(globalOption);
-    QCommandLineOption noDevOption(QStringList() << "no-dev", QObject::tr("Skip installing packages listed in require-dev"));
-    parser.addOption(noDevOption);
+    QCommandLineOption reposOption(QStringList() << "r" << "repo", QObject::tr("Select a repository path as a location for package discovery. E.g. gits=https://github.com."), QObject::tr("type=path"));
+    parser.addOption(reposOption);
+    QCommandLineOption qompoterFileOption(QStringList() << "f" << "file", QObject::tr("If specified, use the given file as qompoter.json file."), QObject::tr("filename"));
+    parser.addOption(qompoterFileOption);
     QCommandLineOption workingDirOption(QStringList() << "d" << "working-dir", QObject::tr("If specified, use the given directory as working directory"), QObject::tr("directory"));
     parser.addOption(workingDirOption);
-    QCommandLineOption vendorDirOption(QStringList() << "vendor-dir", QObject::tr("If specified, use the given directory as vendor directory"), QObject::tr("directory"));
+    QCommandLineOption vendorDirOption(QStringList() << "vendor-dir", QObject::tr("If specified, use the given directory as vendor directory."), QObject::tr("directory"));
     parser.addOption(vendorDirOption);
-    QCommandLineOption qompoterFileOption(QStringList() << "f" << "file", QObject::tr("If specified, use the given file as qompoter.json file"), QObject::tr("filename"));
-    parser.addOption(qompoterFileOption);
+    QCommandLineOption noDevOption(QStringList()<< "no-dev", QObject::tr("Skip packages listed in require-dev."));
+    parser.addOption(noDevOption);
+    QCommandLineOption globalOption(QStringList() << "g" << "global", QObject::tr("Install the packages globaly on the machine instead of localy."));
+    parser.addOption(globalOption);
+    QCommandLineOption verboseOption(QStringList() << "V" << "verbose", QObject::tr("Increase verbosity of messages."));
+    parser.addOption(verboseOption);
+    const QCommandLineOption versionOption = parser.addVersionOption();
+    const QCommandLineOption helpOption = parser.addHelpOption();
 
     if (!parser.parse(QCoreApplication::arguments())) {
         errorMessage = parser.errorText();
@@ -40,18 +42,21 @@ CommandLineParseResult Qompoter::parseCommandLine(QCommandLineParser &parser, Qu
     if (parser.isSet(helpOption))
         return CommandLineHelpRequested;
 
+    if (parser.isSet(reposOption)) {
+        query.addRepository(parser.value(reposOption).section("=", 1), parser.value(reposOption).section("=", 0));
+    }
+    if (parser.isSet(qompoterFileOption)) {
+        query.setQompoterFile(parser.value(qompoterFileOption));
+    }
     if (parser.isSet(workingDirOption)) {
         query.setWorkingDir(parser.value(workingDirOption));
     }
     if (parser.isSet(vendorDirOption)) {
         query.setVendorDir(parser.value(vendorDirOption));
     }
-    if (parser.isSet(qompoterFileOption)) {
-        query.setQompoterFile(parser.value(qompoterFileOption));
-    }
-    query.setVerbose(parser.isSet(verboseOption));
     query.setDev(!parser.isSet(noDevOption));
     query.setGlobal(parser.isSet(globalOption));
+    query.setVerbose(parser.isSet(verboseOption));
 
     const QStringList positionalArguments = parser.positionalArguments();
     if (positionalArguments.isEmpty()) {
