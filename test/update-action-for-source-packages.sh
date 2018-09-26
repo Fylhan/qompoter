775ae3a2517bf10d693e4b2d656f9f6031a57467
Olivier Maridat <olivier.maridat@trialog.com>
2018-09-26 21:29:48 +0200

    Add unit tests regarding update action

--- test/update-action-for-source-packages.sh
+++ test/update-action-for-source-packages.sh
@@ -6,6 +6,27 @@ i=0
 offline=$1
 tests=$(ls ${name}/*${offline}.json | wc -l)
 echo "1..${tests##* }"
+
+echo "Create fake Git repository"
+REPO=qompoter-repo
+mkdir "${REPO}/git"
+mkdir "${REPO}/git/acme-git"
+mkdir "${REPO}/git/acme-git/tmp"
+cd "${REPO}/git/acme-git/tmp"
+touch qompoter.pri
+git init
+git add qompoter.pri
+git commit -m "First commit"
+git tag v2.0.2
+touch another-file.txt
+git add another-file.txt
+git commit -m "Second commit"
+git tag v2.0.3
+cd ..
+git clone --bare tmp acme-git.git
+rm -rf tmp
+cd ../../../
+
 for input in ${name}/*${offline}.json
 do
   i=$((i+1))
@@ -20,5 +41,6 @@ do
   rm -rf vendor
 done
 rm update/but-update-because-no-lock-file-offline.lock
+rm -rf "${REPO}/git"
 #~ echo "$fails test(s) failed"
 exit $fails
